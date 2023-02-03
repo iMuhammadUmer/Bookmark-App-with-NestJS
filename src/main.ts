f2626da05bfcac17e6fc7b6c38e14cf53fdf2a2f
Muhammad Umer <mumer1297@gmail.com>
2023-02-03 18:03:41 +0500

    use ValidationPipe & whitelist: true

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // prevent to send data which is useless for the request
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
