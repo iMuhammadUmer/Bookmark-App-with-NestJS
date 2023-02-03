312d939479d7c3db583c435297c176ccbc8fd379
Muhammad Umer <mumer1297@gmail.com>
2023-02-03 19:20:45 +0500

    add validation for unique signup email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
+import { PrismaClient } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { AuthDto } from '../dto';
 import * as argon from 'argon2';
 
@@ -10,22 +12,31 @@ export class AuthService {
   async signup(dto: AuthDto) {
     // generate password hash
     const hash = await argon.hash(dto.password);
-
-    // save new user in db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        password: hash,
-      },
-      select: {
-        // use select: {} to display desired keys if success
-        id: true,
-        email: true,
-      },
-    });
-    // return saved user
-    return user;
+    try {
+      // save new user in db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+        select: {
+          // use select: {} to display desired keys if success
+          id: true,
+          email: true,
+        },
+      });
+      // return saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
+
   signin() {
     return 'User signed in';
   }
