8fd3f4513c727b49a992dbe66f47a0bb64dd48bf
Muhammad Umer <mumer1297@gmail.com>
2023-02-09 17:30:38 +0500

    add validate() to jwt.strategy.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,25 @@
 import { AuthDto } from './dto';
 import { AuthService } from './auth.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+} from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto:AuthDto) {
+  signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
 
   @Post('signin')
-  signin(@Body() dto:AuthDto) {
+  signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Req } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common/decorators/core/use-guards.decorator';
 import { Get } from '@nestjs/common/decorators/http/request-mapping.decorator';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Controller('users')
 export class UserController {
+  @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  getMe() {
+  getMe(@Req() req: Request) {
+    console.log({
+      user: req.user,
+    });
+
     return 'user info';
   }
 }
