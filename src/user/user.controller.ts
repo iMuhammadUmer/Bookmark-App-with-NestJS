40bb917d4adba8c83172b68a7a23bb67614f78c2
Muhammad Umer <mumer1297@gmail.com>
2023-02-09 16:47:06 +0500

    create controller for user

---
+++ src/user/user.controller.ts
@@ -0,0 +1,10 @@
+import { Controller } from '@nestjs/common';
+import { Get } from '@nestjs/common/decorators/http/request-mapping.decorator';
+
+@Controller('users')
+export class UserController {
+  @Get('me')
+  getMe() {
+    return 'user info';
+  }
+}
